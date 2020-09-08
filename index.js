e922451203b955bff2138498be8d2ec7b7dee1cd
Xiaoyi Liu <lxy878@gmail.com>
2020-09-08 22:10:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function BoardMember(name, homeState, training){
   this.name = name;
   this.homeState = homeState;
   this.training = training;
-  this.g = ()=>{return this.name}
 }
 
 BoardMember.prototype.veto = () => {
