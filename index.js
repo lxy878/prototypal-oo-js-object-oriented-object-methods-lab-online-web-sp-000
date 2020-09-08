bc55b96e60f5901e6c57907e4651f41bc3a4177e
Xiaoyi Liu <lxy878@gmail.com>
2020-09-08 22:06:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ BoardMember.prototype.releasePressStatement = () => {
 }
 
 BoardMember.prototype.sayHi = () => {
-  return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`
+  return `Hi, my name is ${name}. I am from ${homestate}, and I was trained in ${this.training}.`
 }
 
 const c = new BoardMember("Mr. Polished", "New York", "law");
